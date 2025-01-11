43771e0e0fb3e38919eead78f0891b89684e7628
Saumya Roy <saumyashovanroy@gmail.com>
2025-01-11 01:57:41 -0500

    Generate Report

--- MyAssistantApp/main.py
+++ MyAssistantApp/main.py
@@ -21,6 +21,13 @@ class Password(SQLModel, table=True):
     username: str
     password: str
 
+# Notes
+class Note(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    title: str
+    value: str
+
+
 class Task(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     title: str
@@ -44,16 +51,13 @@ def initial_data():
     task_1 = Task(title="Task1", description="This is Task 1")
     task_2 = Task(title="Task2", description="This is Task 2")
     task_3 = Task(title="Task3", description="This is Task 3")
-
+    note_1 = Note(title="Note 1", value="Destination: Paris\nDates: 2023-06-01 to 2023-06-10\nBookings:\n- Flight on 2023-05-31, return on 2023-06-11\n- Hotel: City Center Hotel, booking ID: 987654321, Meeting Date: 2023-01-15\nAttendees: Alice, Bob, Charlie\nDiscussion:\n- Budget review\n- Project timelines update\n- New hiring proposals, Gmail: Alu1, password123\nBank Account: potol2, password456\nNetflix: user3, pass789")
+    note_2 = Note(title="Note 2", value="Alu1: password123, potol2: password456")
 
     with Session(engine) as session:
-        session.add(password_1)
-        session.add(password_2)
-        session.add(task_1)
-        session.add(task_2)
-        session.add(task_3)
-
+        session.add_all([password_1, password_2, task_1, task_2, task_3, note_1, note_2])
         session.commit()
+
 
 
 def get_all_tasks(session: SessionDep) -> Sequence[Task]:
@@ -62,6 +66,9 @@ def get_all_tasks(session: SessionDep) -> Sequence[Task]:
 
 def get_all_passwords(session: SessionDep) -> Sequence[Password]:
     return session.exec(select(Password)).all()
+
+def get_all_notes(session: SessionDep) -> Sequence[Note]:
+    return session.exec(select(Note)).all()
 
 @asynccontextmanager
 async def lifespan(current_app: FastAPI):
@@ -88,4 +95,6 @@ def read_root(session: SessionDep, api_key: str = Depends(get_api_key)) -> Seque
 def read_root(session: SessionDep, api_key: str = Depends(get_api_key)) -> Sequence[Task]:
     return get_all_tasks(session)
 
-
+@app.get("/notes")
+def read_root(session: SessionDep, api_key: str = Depends(get_api_key)) -> Sequence[Note]:
+    return get_all_notes(session)
